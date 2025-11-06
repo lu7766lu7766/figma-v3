7899158c6b49126a8e62be17a5f955b5eb166204
Jac Wang <lu7766lu7766@gmail.com>
2025-11-06 16:58:33 +0800

    ref, computed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <h1>You did it!</h1>
   <HelloWorld />
+  <div>count: {{ count }}</div>
+  <div>double: {{ double }}</div>
 </template>
 
 <script setup lang="ts">
 import HelloWorld from "./hello_world.vue"
+import { ref, computed } from "vue"
+let count = ref(0)
+let double = computed(() => count.value * 2)
+// let double = computed(function () {
+//   return count.value * 2
+// })
+setInterval(() => {
+  count.value++
+}, 1000)
 </script>
 
 <style scoped></style>
