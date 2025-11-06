12db9d8796834b971733c6540edeb33372029d10
Jac Wang <lu7766lu7766@gmail.com>
2025-11-06 16:26:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>You did it!</h1>
+  <HelloWorld />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "./hello_world.vue"
+</script>
+
+<style scoped></style>
---
+++ src/hello_world.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="red">hello world</h1>
+</template>
+
+<style scoped>
+.red {
+  color: blueviolet;
+}
+</style>
